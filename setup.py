146f5a9ec17530b2517c620becff263d8db19938
gldeng <realgldeng@gmail.com>
2024-03-05 12:40:53 +0800

    Add requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='sdr_utils',
     version='0.1',
     packages=find_packages(),
     license='MIT',
     long_description=open('README.md').read(),
-    install_requires=[
-        # Add your library's dependencies here
-    ],
+    install_requires=required,
 )
